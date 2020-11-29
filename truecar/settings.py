7df4ad5a94061311f871f80e98843c01a3eed094
Dylan McClain <dmcclain@paydici.com>
2020-11-29 09:29:16 -0800

    define user agent for spider

--- truecar/settings.py
+++ truecar/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'truecar.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'scrapy'
+USER_AGENT = 'truecar Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
